c6e10384c441841aaee5d38ad2ca1389f872136c
hasanrahman2503 <hasanrahman2503@gmail.com>
2024-12-22 12:57:01 +0000

    Add files via upload

---
+++ Graph_R_code.R
@@ -0,0 +1,26 @@
+library(ggplot2)
+# Sample data
+df <- data.frame(
+  Hotstreak = c("0","1","2","3","4","5+"),
+  Games = c(24002,8918,3657,1712,882,496),
+  cock=c("0","1","2","3","4","5+")
+)
+
+# Create a ggplot bar graph without gaps on both axes
+ggplot(df, aes(x = Hotstreak, y = Games, fill = cock)) +
+  geom_bar(stat = "identity",width=1) + 
+  scale_x_discrete(expand = c(0, 0)) +  # Remove gap on x-axis
+  scale_y_continuous(expand = c(0, 0)) +  # Remove gap on y-axis+
+  scale_fill_manual(values=c("lightseagreen",
+                            "lightblue", 
+                            "seagreen1", 
+                            "thistle",
+                            "peachpuff",
+                            "salmon")) +
+  theme(legend.position = "none")+
+  theme(panel.background = element_blank())+
+  theme(
+    axis.title.x = element_text(family = "sans", face = "bold", size = 14), # X-axis title properties
+    axis.title.y = element_text(family = "sans", face = "bold", size = 14))+
+  labs(x = "Hot streak length", y = "Matches")
+
