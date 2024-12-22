6466f72ed0fef7575171c26d5928c86b84ef5976
hasanrahman2503 <hasanrahman2503@gmail.com>
2024-12-22 13:10:47 +0000

    Add files via upload

---
+++ Predictive model.R
@@ -0,0 +1,3 @@
+data = read.csv("Pastmatches.csv", header = TRUE)
+model<-glm(result~matchresult1+matchresult2+matchresult3+matchresult1:matchresult2+matchresult1:matchresult2:matchresult3,data=data,family = "binomial")
+summary(model)
